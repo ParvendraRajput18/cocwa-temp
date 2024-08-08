0c66d38ff4fe7d959053a120844d94d9311ea229
ParvendraRajput18 <Parvendra.Rajput@adixoo.com>
2024-08-08 18:55:19 +0530

    Added the required apis

--- src/controllers/homePage.ts
+++ src/controllers/homePage.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 
 export const homePage = (req: Request, res: Response) => {
-  const message: string = "router working ";
+  const message: string = "Api is working!";
   res.send(message);
 };
